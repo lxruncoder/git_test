69d0d7d884e8cafbc1f36e396ac2c18dedec1202
lxruncoder <247059750@qq.com>
2023-06-04 14:08:53 +0800

    Update CarMapper.java

--- src/main/java/com/lx/CarMapper.java
+++ src/main/java/com/lx/CarMapper.java
@@ -3,5 +3,6 @@ package com.lx;
 public class CarMapper {
     public static void main(String[] args) {
         System.out.println("hello world");
+         System.out.println("hello world");
     }
 }
