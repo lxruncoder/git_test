27812e97152b7e0b8baff8ce3855af93fb7de54a
lx <247059750@qq.com>
2023-06-04 11:38:17 +0800

    second commit

--- src/main/java/com/lx/UserMapper.java
+++ src/main/java/com/lx/UserMapper.java
@@ -3,5 +3,6 @@ package com.lx;
 public class UserMapper {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
